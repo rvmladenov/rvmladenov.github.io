7dedfddb42d5fcdc9ae0c1f98eac899c34144e63
Radoslav Mladenov <rvmladenov@gmail.com>
2014-05-15 11:29:45 +0300

    added descriptions

--- softuni/Java Basics/Level 1/homeworks/SortArrayOfStrings.java
+++ softuni/Java Basics/Level 1/homeworks/SortArrayOfStrings.java
@@ -10,8 +10,14 @@ public class SortArrayOfStrings {
 		
 		String[] strArr = new String[number];
 
+		// Kogato ispolzvame "n.nextLine();" to 4ete vsi4ko do kraq na reda, no 
+		// ako predi tova sme izpolzvali naprimer "n.nextInt();", to tova "do kraq na reda" e realno do
+		// kraq na na vavedenata stoinost. I zatova e nujen ssledva6tiq red
+		n.nextLine();
 		for (int i = 0; i < number; i++) {
-			strArr[i] = n.next("\\w+");
+			// TODO: Play with the reg expressions
+			// strArr[i] = n.nextLine("\\w+");
+			strArr[i] = n.nextLine("\\w+");
 		}
 		n.close();
 		
