389c1dbb73e58e5e251ca6e04551b5f2ed6329fd
rvmladenov <rvmladenov@gmail.com>
2014-05-15 21:36:59 +0300

    fixed some staff

--- softuni/Java Basics/Level 1/homeworks/SortArrayOfStrings.java
+++ softuni/Java Basics/Level 1/homeworks/SortArrayOfStrings.java
@@ -5,28 +5,22 @@ import java.util.Scanner;
 public class SortArrayOfStrings {
 
 	public static void main(String[] args) {
-		Scanner n = new Scanner(System.in);
-		int number = n.nextInt();
-		
-		String[] strArr = new String[number];
+		try(Scanner n = new Scanner(System.in)) {
 
-		// Kogato ispolzvame "n.nextLine();" to 4ete vsi4ko do kraq na reda, no 
-		// ako predi tova sme izpolzvali naprimer "n.nextInt();", to tova "do kraq na reda" e realno do
-		// kraq na na vavedenata stoinost. I zatova e nujen ssledva6tiq red
-		n.nextLine();
-		for (int i = 0; i < number; i++) {
-			// TODO: Play with the reg expressions
-			// strArr[i] = n.nextLine("\\w+");
-			strArr[i] = n.nextLine("\\w+");
+			int number = Integer.parseInt(n.nextLine());
+			
+			String[] strArr = new String[number];
+	
+			for (int i = 0; i < number; i++) {
+				strArr[i] = n.nextLine();
+			}
+			
+			Arrays.sort(strArr);
+
+			for (int i = 0; i < number; i++) {
+				System.out.println(strArr[i]);
+			}
 		}
-		n.close();
-		
-		Arrays.sort(strArr);
-		
-		for (int i = 0; i < number; i++) {
-			System.out.println(strArr[i]);
-		}
-		
 	}
 
 }
