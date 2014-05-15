e14e9e5aa0acbf13fe702d41d1211ac413c4b0de
rvmladenov <rvmladenov@gmail.com>
2014-05-15 22:35:27 +0300

    added solution for the first problem

--- softuni/Java Basics/Level 1/homeworks/week2/src/Problem1_RectangleArea.java
+++ softuni/Java Basics/Level 1/homeworks/week2/src/Problem1_RectangleArea.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 
 public class Problem1_RectangleArea {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try(Scanner sc = new Scanner(System.in)) {
+			System.out.print("a = ");
+			int a = Integer.parseInt(sc.nextLine());
+			
+			System.out.print("b = ");
+			int b = Integer.parseInt(sc.nextLine());
+			
+			System.out.printf("area = %1$s", (a*b));
+		}
 	}
 
 }
