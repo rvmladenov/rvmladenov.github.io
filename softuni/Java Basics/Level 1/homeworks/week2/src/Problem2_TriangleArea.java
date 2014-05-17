af46c5ef82992f6f73c8dc35ba2a321e699dfc0b
rvmladenov <rvmladenov@gmail.com>
2014-05-17 19:12:39 +0300

    Added the solution for Problem 2

--- softuni/Java Basics/Level 1/homeworks/week2/src/Problem2_TriangleArea.java
+++ softuni/Java Basics/Level 1/homeworks/week2/src/Problem2_TriangleArea.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class Problem2_TriangleArea {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try(Scanner sc = new Scanner(System.in)) {
+			System.out.print("Ax = ");
+			int Ax = sc.nextInt();
+			System.out.print("Ay = ");
+			int Ay = sc.nextInt();
+			
+			System.out.print("Bx = ");
+			int Bx = sc.nextInt();
+			System.out.print("By = ");
+			int By = sc.nextInt();
+			
+			System.out.print("Cx = ");
+			int Cx = sc.nextInt();
+			System.out.print("Cy = ");
+			int Cy = sc.nextInt();
+			
+			int area = Math.abs(((Ax*(By - Cy)) + (Bx*(Cy - Ay)) + (Cx*(Ay - By)))/2);
+			
+			System.out.printf("Area = %1$s", area);
+		}
 	}
 
 }
