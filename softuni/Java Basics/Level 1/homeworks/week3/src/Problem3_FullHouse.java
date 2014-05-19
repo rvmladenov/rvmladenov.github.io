9df0fa0492bc23c23f701f9887680922d6cc19bb
Radoslav Mladenov <rvmladenov@gmail.com>
2014-05-19 22:33:48 +0300

    Added parial solutions for Problems 3 & 6

---
+++ softuni/Java Basics/Level 1/homeworks/week3/src/Problem3_FullHouse.java
@@ -0,0 +1,14 @@
+public class Problem3_FullHouse {
+
+	public static void main(String[] args) {
+		
+		char spato = '\u2663';
+		char karo = '\u2666';
+		char kupa = '\u2665';
+		char pika = '\u2660';
+		
+		String[] faces = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		char[] suits = {spato, karo, kupa, pika};
+	}
+
+}
---
+++ softuni/Java Basics/Level 1/homeworks/week3/src/Problem6_RandomHandsOf5Cards.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Problem6_RandomHandsOf5Cards {
+
+	public static void main(String[] args) {
+		
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			int n = sc.nextInt();
+			for (int i = 0; i < n; i++) {
+				System.out.printf("%1$s %2$s %3$s %4$s %5$s");
+			}
+		}
+	}
+	
+	public static String getRandCards() {
+		String result = "";
+		ArrayList<String> suitOfCards = getSuitOfCards();
+		
+		ArrayList<Integer> previousChoices = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			int randNum = randInt();
+			while (previousChoices.contains(randNum)) {
+				randNum = randInt();
+			}
+
+			String card = suitOfCards.get(randNum);
+			previousChoices.add(randNum);
+			result += " " + card;
+		}
+		
+		return result;
+	}
+	
+	public static int randInt() {
+		int min = 1;
+		int max = 52;
+	    Random rand = new Random();
+	    
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+	    return randomNum;
+	}
+	
+	public static ArrayList<String> getSuitOfCards() {
+		
+		ArrayList<String> resultArr = new ArrayList<>();
+		
+		char[] suits = {'\u2663', '\u2666', '\u2665', '\u2660'};
+		String[] faces = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		
+		for (int i = 0; i < faces.length; i++) {
+			for (int j = 0; j < suits.length; j++) {
+				resultArr.add( faces[i] + suits[j] );
+			}
+		}
+		
+		return resultArr;
+	}
+
+}
