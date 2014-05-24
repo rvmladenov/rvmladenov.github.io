3c04c525786b82226b82eb5171f50b21764ecd6a
rvmladenov <rvmladenov@gmail.com>
2014-05-24 17:01:34 +0300

    Added solution for problem 2

---
+++ softuni/Java Basics/Level 1/homeworks/week4/src/Problem2_SequencesOfEqualStrings.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problem2_SequencesOfEqualStrings {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			String input = sc.nextLine();
+			String[] splited = input.split(" ");
+			
+			for (int i = 0; i < splited.length - 1; i++) {
+				// Оставаме последния елемент, който е "splited.length - 1" да се обработва последно
+				
+				String whatToPrint = splited[i] + " ";
+				if(!splited[i].equals(splited[i + 1])) {
+					whatToPrint += "\n";
+				}
+				System.out.printf("%s", whatToPrint);
+			}
+			
+			System.out.printf("%s", splited[splited.length - 1]);
+		}
+	}
+
+}
