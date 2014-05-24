7a1ef9385df1d28ac318913dc3007ff349949a32
rvmladenov <rvmladenov@gmail.com>
2014-05-24 17:01:34 +0300

    Testing linux/windows rep updates

--- softuni/Java Basics/Level 1/homeworks/week4/src/Problem1_SortArrayOfNumbers.java
+++ softuni/Java Basics/Level 1/homeworks/week4/src/Problem1_SortArrayOfNumbers.java
@@ -8,7 +8,7 @@ public class Problem1_SortArrayOfNumbers {
 		
 		try(Scanner sc = new Scanner(System.in)) {
 			int n = sc.nextInt();
-			int[] intArr = new int[n];
+			int[] intArr = new int[n]; 
 			
 			for (int i = 0; i < n; i++) {
 				intArr[i] = sc.nextInt();
