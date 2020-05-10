7e0e7dc48acd9a618d4f26cda191d0dd807e9cc5
Radoslav Mladenov <rmladenov@soft2run.com>
2020-05-10 11:37:44 +0300

    temporary moved to the index

---
+++ precache-manifest.cb8260a3644aa881d96e60c8cf40ad03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cfd82576db44ce9fdb4ba999f1f66a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51212f7a1e0a6033981",
+    "url": "/static/css/main.5c899595.chunk.css"
+  },
+  {
+    "revision": "cce470450eda7fefd865",
+    "url": "/static/js/2.d1666ffd.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.d1666ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51212f7a1e0a6033981",
+    "url": "/static/js/main.d6711ba6.chunk.js"
+  },
+  {
+    "revision": "4d603119b1122a69b391",
+    "url": "/static/js/runtime-main.13c76dad.js"
+  }
+]);
